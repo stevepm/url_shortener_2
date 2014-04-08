6a612163906eb164200227759a6f79cf400f9d70
stevepm <steven.magelowitz@gmail.com>
2014-04-08 15:42:13 -0600

    Added class to create new Urls instance with a database

--- url_shortener.rb
+++ url_shortener.rb
@@ -1,12 +1,14 @@
 require 'sinatra/base'
 require 'uri'
-require './urls'
+require_relative 'urls'
 
 class UrlShortener < Sinatra::Application
 
-  URLS = Urls.new
   INVALID_URL_ERROR = 'Please+enter+a+valid+URL'
 
+  class << self
+    attr_accessor :urls
+  end
 
   get '/' do
     error = params[:error]
@@ -19,7 +21,7 @@ class UrlShortener < Sinatra::Application
     if url_to_shorten.empty? || url_to_shorten.split(' ').count > 1 || !is_valid_url?(url_to_shorten) || url_to_shorten.nil?
       redirect "/?url=#{url_to_shorten}&error=#{INVALID_URL_ERROR}"
     else
-      id = URLS.add(url_to_shorten)
+      id = self.class.urls.add(url_to_shorten)
       redirect to("/#{id}?stats=true")
     end
 
@@ -28,18 +30,20 @@ class UrlShortener < Sinatra::Application
   get '/:id' do
     stats = params[:stats] == 'true'
     id = params[:id]
-    original_url = URLS.find_url(id)
+    original_url = self.class.urls.find_url(id)
     new_url = "#{request.base_url}/#{id}"
-    views = URLS.find_stats(id)
+    views = self.class.urls.find_stats(id)
     if stats
       erb :stats, :locals => {:original_url => original_url, :new_url => new_url, :views => views}
     else
-      URLS.increase_views(id)
+      self.class.urls.increase_views(id)
       redirect original_url
     end
   end
 
+  private
   def is_valid_url?(url_to_shorten)
+    # url_to_shorten.prepend('http://') unless url_to_shorten.include('http://','https://')
     url = false
     if url_to_shorten =~ /^#{URI::regexp}$/
       url = true
